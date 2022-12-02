d2b409137fcefbd30cc8aa697d01b0babd7ed31a
Oleksii Muakhrovskyi <a.mukahorvskiy@gmail.com>
2022-12-02 22:43:06 +0100

    init backend for CV

---
+++ src/server.ts
@@ -0,0 +1,40 @@
+import {ApolloServer} from '@apollo/server';
+import {expressMiddleware} from '@apollo/server/express4';
+import {ApolloServerPluginDrainHttpServer} from '@apollo/server/plugin/drainHttpServer';
+import express from 'express';
+import http from 'http';
+import {json} from 'body-parser';
+import cors from 'cors';
+import {resolvers, typeDefs} from "./graphql";
+
+interface MyContext {
+    token?: String;
+}
+
+async function startApolloServer() {
+    const PORT = process.env.PORT || 4000;
+
+    const app = express();
+    const httpServer = http.createServer(app);
+    const server = new ApolloServer<MyContext>({
+        typeDefs,
+        resolvers,
+        plugins: [ApolloServerPluginDrainHttpServer({httpServer})],
+    });
+
+    await server.start();
+
+    app.use(
+        '/graphql',
+        cors<cors.CorsRequest>(),
+        json(),
+        expressMiddleware(server, {
+            context: async ({req}) => ({token: req.headers.token}),
+        }),
+    );
+
+    await new Promise<void>((resolve) => httpServer.listen({port: PORT}, resolve));
+    console.log(`🚀 Server ready at http://localhost:4000/graphql`);
+}
+
+startApolloServer()
