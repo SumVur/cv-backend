cffce5206ec02718ecd873d7fac156f37d06f0ba
Oleksii Muakhrovskyi <a.mukahorvskiy@gmail.com>
2022-12-10 18:59:23 +0100

    add redis

--- src/Model/api/index.ts
+++ src/Model/api/index.ts
@@ -1,9 +1,21 @@
 import {ModelInterface} from "./modelInterface";
 import {CollectionInterface} from "./collectionInterface";
+import redis from "../../helper";
 
 export * from "./repositoryInterface"
 export * from "./modelInterface"
 export * from "./collectionInterface"
 
 export interface ResourceInterface<T> extends ModelInterface<T>, CollectionInterface<T> {
+}
+
+export async function redisGetOrSave(key: string, fetcher: () => Promise<any>) {
+  const data = await redis.get(key);
+  if (!data) {
+    const response = await fetcher();
+    await redis.set(key, JSON.stringify(response));
+    return response;
+  } else {
+    return JSON.parse(data);
+  }
 }
--- src/helper/index.ts
+++ src/helper/index.ts
@@ -1,4 +1,6 @@
 import {createClient} from "@supabase/supabase-js";
+import { createClient as redisCreateClient } from "redis";
+
 import dotenv from "dotenv";
 
 dotenv.config()
@@ -7,3 +9,8 @@ const supabaseUrl = process.env.PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(`${supabaseUrl}`, `${supabaseKey}`);
+
+const url = process.env.REDIS_URL;
+
+const redis = redisCreateClient({ url });
+export default redis;
